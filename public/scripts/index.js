942d65bed568a110af074dee86585ad5bebaca6d
dheyme-jensen <dheymejensen@hotmail.com>
2019-05-19 21:12:17 -0300

    unfinished swipe function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,25 +1,28 @@
 const gifBase = document.querySelector('.gifBase');
 const dataBase = firebase.database();
+const index = 0;
 
-window.onload = () => {
+function getGifOnApi() {
     requestAPI.random
         .then(response => response.json())
-        .then(response => {
-            const { data } = response;
-            gifBase.innerHTML = gifOptions.image(data)
+        .then(response => gifsOnScreenAndDatabase(response.data));
+}
 
-            const image = document.querySelector('.image-gif')
-            image.addEventListener('click', event => {
-                dataBase.ref("favoriteGifs/").push({
-                    id: data.id,
-                    url: data.images.fixed_width_small.url,
-                })
-                .then(function () {
-                    console.log("Document successfully written!");
-                })
-                .catch(function (error) {
-                    console.error("Error writing document: ", error);
-                });
-            })
+function gifsOnScreenAndDatabase(data) {
+    gifBase.innerHTML = gifOptions.image(data[index])
+    index++;
+
+    const image = document.querySelector('.image-gif')
+    image.addEventListener('click', event => {
+        dataBase.ref("favoriteGifs/").push({
+            id: data.id,
+            url: data.images.fixed_width_small.url,
         })
+            .then(function () {
+                console.log("Document successfully written!");
+            })
+            .catch(function (error) {
+                console.error("Error writing document: ", error);
+            });
+    })
 }
