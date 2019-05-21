f2e5669f278e9f7fa4f99d7201a432e67074a7a7
Jéssica Happatsch <vonhappatsch@gmail.com>
2019-05-21 15:56:18 -0300

    [JS] [HTML] register screen

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -7,79 +7,79 @@ const emailInput = document.querySelector("#signUpEmail");
 const passwordInput = document.querySelector("#signUpPassword");
 
 window.onload = () => {
-  typing();
-  signUpBtn.addEventListener('click', signUp);
-  ggglBtn.addEventListener('click', signUpGoogle);
-  fbBtn.addEventListener('click', signUpFacebook);
+    typing();
+    signUpBtn.addEventListener('click', signUp);
+    ggglBtn.addEventListener('click', signUpGoogle);
+    fbBtn.addEventListener('click', signUpFacebook);
 }
 
 function manage(email, password) {
-  if (email.value !== '' && password.value !== '') {
-    signUpBtn.disabled = false;
-  } else {
-    signUpBtn.disabled = true;
-  }
+    if (email.value !== '' && password.value !== '') {
+        signUpBtn.disabled = false;
+    } else {
+        signUpBtn.disabled = true;
+    }
 }
 
 function typing() {
-  let inputs = [emailInput, passwordInput];
-  inputs.forEach(function (el) {
-    el.addEventListener("keyup", function () {
-      manage(emailInput, passwordInput);
+    let inputs = [emailInput, passwordInput];
+    inputs.forEach(function(el) {
+        el.addEventListener("keyup", function() {
+            manage(emailInput, passwordInput);
+        });
     });
-  });
 }
 
 function signUp(e) {
-  e.preventDefault();
-  const email = emailInput.value;
-  const password = passwordInput.value;
-  createUser(email, password);
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    createUser(email, password);
 }
 
 function createUser(email, password) {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function (response) {
-      const userId = response.user.uid;
-      database.ref('users/' + userId).set({
-        email: email
-      });
-      createProfile(userId);
-    })
-    .catch(function (error) {
-      handleErrors(error)
-    });
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(response) {
+            const userId = response.user.uid;
+            database.ref('users/' + userId).set({
+                email: email
+            });
+            createProfile(userId);
+        })
+        .catch(function(error) {
+            handleErrors(error)
+        });
 }
 
 function signUpGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  signInWithPopup(provider);
+    const provider = new firebase.auth.GoogleAuthProvider();
+    signInWithPopup(provider);
 }
 
 function signUpFacebook() {
-  const provider = new firebase.auth.FacebookAuthProvider();
-  signInWithPopup(provider);
+    const provider = new firebase.auth.FacebookAuthProvider();
+    signInWithPopup(provider);
 }
 
 function signInWithPopup(provider) {
-  firebase.auth().signInWithPopup(provider).then(function (response) {
-    // const token = response.credential.accessToken;
-    const userId = response.user;
-    createProfile(userId);
-  }).catch(function (error) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.email;
-    const credential = error.credential;
-    alert(errorCode, errorMessage, email, credential);
-  });
+    firebase.auth().signInWithPopup(provider).then(function(response) {
+        // const token = response.credential.accessToken;
+        const userId = response.user;
+        createProfile(userId);
+    }).catch(function(error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        alert(errorCode, errorMessage, email, credential);
+    });
 }
 
 function handleErrors(error) {
-  const errorMessage = error.message;
-  alert(errorMessage);
+    const errorMessage = error.message;
+    alert(errorMessage);
 }
 
 function createProfile(userId) {
-  window.location = `create-profile.html?id=${userId}`;
+    window.location = `create-profile.html?id=${userId}`;
 }
